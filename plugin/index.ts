ee4d5ccb7bf89c02ba5c77b6f338e246476f005e
Fábio Manuel Serra Araújo <fabio.ms.araujo@gmail.com>
2017-05-26 17:39:06 +0100

    Update comment of config property 'ILoginConfiguration.google.isRequestAuthCode' to reflect change of default value

--- plugin/index.ts
+++ plugin/index.ts
@@ -78,8 +78,8 @@ export interface ILoginConfiguration {
         serverClientId?: string;
 
         /**
-         * If true (default), it will request for offline auth code which server can use for fetching or refreshing auth tokens. It will be set in authCode property of result object.
-         * If false, it will request for token id. it will be set in authToken property of result object.
+         * If true, it will request for offline auth code which server can use for fetching or refreshing auth tokens. It will be set in authCode property of result object.
+         * If false (default), it will request for token id. it will be set in authToken property of result object.
          */
         isRequestAuthCode?: boolean;
     }
