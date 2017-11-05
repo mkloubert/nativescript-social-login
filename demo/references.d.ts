f71b83e1d6f48294b1a0f33cf39af7625aedef73
Jogboms <jeremiahogbomo@gmail.com>
2017-11-05 07:21:19 +0100

    chore(cleanup): code clean up

--- demo/references.d.ts
+++ demo/references.d.ts
@@ -1,7 +1,2 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-
-declare type FormData = any;
-declare var FormData;
-declare type Blob = any;
-declare var Blob;
