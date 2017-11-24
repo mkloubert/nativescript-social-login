e598f0396e4974c1cc4e6586cd291e475f88e482
Jogboms <jeremiahogbomo@gmail.com>
2017-11-24 16:57:43 +0100

    refactor(utils):  allow merge for Array parameters
    Fixes #26

--- plugin/utils.ts
+++ plugin/utils.ts
@@ -4,7 +4,9 @@
 export function merge(to: Object, from: Object) {
     const copy = Object.assign({}, to);
     for (const n in from) {
-        if (typeof to[n] !== "object") {
+        if (Array.isArray(to[n])) {
+            copy[n] = copy[n].concat(from[n].filter(el => copy[n].indexOf(el) === -1));
+        } else if (typeof to[n] !== "object") {
             copy[n] = from[n];
         } else if (typeof from[n] === "object") {
             copy[n] = merge(copy[n], from[n]);
