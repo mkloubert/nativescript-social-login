63b1c7f228c3215e5bca32033dcb86b03cf8792f
Jogboms <jeremiahogbomo@gmail.com>
2018-01-03 13:22:21 +0100

    revert(android): 3 digit facebook request code

--- plugin/SocialLogin.android.ts
+++ plugin/SocialLogin.android.ts
@@ -39,7 +39,7 @@ const actionRunnable = java.lang.Runnable.extend({
 
 export class SocialLogin extends Social {
     private _rcGoogleSignIn: number = 597; // < 16 bits
-    private _rcFacebookSignIn: number = 442; // < 16 bits
+    private _rcFacebookSignIn: number = 64206; // < 16 bits
     private _fbCallbackManager;
     private _fbLoginManager;
 
